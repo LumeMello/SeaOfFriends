cad8d363f7ac289bb8220e4a26a2b75f8037933d
LumeMello <123975458+LumeMello@users.noreply.github.com>
2026-07-10 21:52:54 -0300

    Add files via upload

--- Scripts/Characters/CharacterBase.cs
+++ Scripts/Characters/CharacterBase.cs
@@ -1,10 +1,33 @@
-using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Character", menuName = "Character Base")]
-public class CharacterBase : ScriptableObject
+public class CharacterBase : MonoBehaviour
 {
+    [System.Serializable]
+    public class Usages
+    {
+        public CardBase cardId;
+        public int uses;
+
+        public override bool Equals(object other)
+        {
+            if (other == null) return false;
+            Usages otherUsage = other as Usages;
+            if (otherUsage == null || otherUsage.cardId == null) return false;
+            else return Equals(otherUsage);
+        } 
+
+        public override int GetHashCode()
+        {
+            return cardId.cardInfo.myId;
+        }
+
+        public bool Equals(Usages other)
+        {
+            if (other == null) return false;
+            return (this.cardId.cardInfo.myId.Equals(other.cardId.cardInfo.myId));
+        }
+    }
+
     [System.Serializable]
     public class Info
     {
@@ -12,11 +35,31 @@ public class CharacterBase : ScriptableObject
         public int Totallife;
         public int currentLife;
         public int shield;
-        public List<CardBase> itens = new List<CardBase>();
+        public List<Usages> itensUsage = new List<Usages>();
 
         [TextArea(4, 8)]
         public string myDescription;
+
+        public void AddItem(CardBase cardBase, int usages)
+        {
+            Usages use = new Usages();
+            use.cardId = cardBase;
+            use.uses = usages;
+            itensUsage.Add(use);
+        }
+
+        public void RemoveItem(CardBase cardBase)
+        {
+            Usages use = new Usages();
+            use.cardId = cardBase;
+            use.uses = 0;
+            itensUsage.Remove(use);
+        }
     }
 
-    public Info dialogueInfo;
+
+
+    public Info characterInfo;
+
+    
 }
