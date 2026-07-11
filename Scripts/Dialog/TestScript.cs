1a5bc7a8438cacc79fa7ea145383281f3b8c435c
LumeMello <123975458+LumeMello@users.noreply.github.com>
2026-07-10 21:53:24 -0300

    Add files via upload

--- Scripts/Dialog/TestScript.cs
+++ Scripts/Dialog/TestScript.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
+using static CharacterBase;
 
 public class TestScript : MonoBehaviour
 {
     public DialogueBase dialogue;
-
+    public CharacterBase attacker;
+    public CharacterBase defender;
+    private CardBase Item;
+    public int id;
     public void TriggerDialogue()
     {
         DialogueManager.instance.EnqueueDialogue(dialogue);
+    }
+
+    public void TriggerAttack()
+    {
+        CombatManager.instance.AttackAction(attacker,defender,Item);
+    }
+
+    public void TriggerRest()
+    {
+        CombatManager.instance.RestAction(attacker);
     }
 
     private void Update()
@@ -22,6 +36,21 @@ public class TestScript : MonoBehaviour
                 TriggerDialogue();
             }
             
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            foreach (Usages item in attacker.characterInfo.itensUsage)
+            {
+                if (item.cardId.cardInfo.myId == id)
+                {
+                    Item = item.cardId;
+                    TriggerAttack();
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            TriggerRest();
         }
     }
 }
